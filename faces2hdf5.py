8c2f512f9ceab88c0bb33b9e8521bb7efd75adb4
Carlos Agarie <carlos.agarie@gmail.com>
2016-10-10 17:21:23 -0300

    Terminei o script que converte faces.csv em HDF5
    O script `faces2hdf5.py` realiza a conversão de CSV para HDF5 do dataset
    de faces citado no livro "Machine Learning" de Tom Mitchell[1]. Esse
    HDF5 usa o formato adotado pelo Caffe, em que o arquivo possui os
    datasets `data` e `label`.
    
    [1]: https://www.cs.cmu.edu/~tom/faces.html

--- faces2hdf5.py
+++ faces2hdf5.py
@@ -3,27 +3,35 @@ import csv
 import h5py
 import numpy as np
 
+
 def encode_direction(direction):
-    return {
-            "up": [1, 0, 0, 1],
+    return {"up": [1, 0, 0, 0],
             "straight": [0, 1, 0, 0],
             "left": [0, 0, 1, 0],
             "right": [0, 0, 0, 1]
             }[direction]
 
-# Dataset de faces: 624 itens, cada um com 960 atributos (pixels).
+
+def data_and_labels():
+    data = np.zeros((624, 960))
+    labels = np.zeros((624, 4))
+    with open("faces.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        i = 0
+        next(reader)  # Header.
+        for row in reader:
+            data[i, :] = np.array([float(x) for x in row[0:-1]])
+            labels[i, :] = encode_direction(row[-1])
+            i = i + 1
+    return data, labels
+
+data, labels = data_and_labels()
 
 f = h5py.File("faces.h5", "w")
+f.create_dataset("data", data.shape, dtype="f8")
+f.create_dataset("label", labels.shape, dtype="f8")
 
-# Preciso modificar isso para criar datasets de treino/validação.
-f.create_dataset("data", (624, 960), dtype="i8")
-f.create_dataset("label", (624, 4), dtype="f4")
+f["data"][:] = data.astype("f8")
+f["label"][:] = labels.astype("f8")
 
-# Abre o CSV, lê cada linha e popula f['data'] e f['label'].
-with open("faces.csv", "r") as csvfile:
-    reader = csv.reader(csvfile)
-    i = 0
-    for row in reader:
-        f["data"][i] = row[0:-2]
-        f["label"][i] = encode_direction(row[-1])
-        i = i + 1
+f.close()
