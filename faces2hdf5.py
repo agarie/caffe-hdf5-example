98c3ebf48f4cea90894ef19d4edbeeef30afa060
Carlos Agarie <carlos.agarie@gmail.com>
2016-10-10 22:57:58 -0300

    Arrumei um bug no embaralhamento do dataset
    Estava dando `np.random.shuffle` nos pixels e nas labels separadamente,
    gerando um dataset todo errado — não é a toa que os resultados estavam
    tão ruins.

--- faces2hdf5.py
+++ faces2hdf5.py
@@ -8,35 +8,38 @@ def encode_direction(direction):
     return {"up": 1,
             "straight": 2,
             "left": 3,
-            "right": 4
+            "right": 4,
             }[direction]
 
 
-def data_and_labels():
-    data = np.zeros((624, 960))
-    labels = np.zeros((624, 1))
-    with open("faces.csv", "r") as csvfile:
+def data_matrix_from_csv(csv_filename):
+    X = np.zeros((624, 961))
+    with open(csv_filename, "r") as csvfile:
         reader = csv.reader(csvfile)
         i = 0
-        next(reader)  # Header.
+        next(reader)
         for row in reader:
-            data[i, :] = np.array([float(x) for x in row[0:-1]])
-            labels[i, :] = encode_direction(row[-1])
+            X[i, :] = [float(x) for x in row[0:-1]] + [encode_direction(row[-1])]
             i = i + 1
+    return X
+
+
+def data_and_labels(data_matrix):
+    np.random.shuffle(data_matrix)
+    data = data_matrix[:, 0:-1]
+    labels = data_matrix[:, -1]
     return data, labels
 
 
 def save_dataset_with(filename, data, labels):
     f = h5py.File(filename, "w")
     f.create_dataset("data", data.shape, dtype="f8")
-    f.create_dataset("label", labels.shape, dtype="f8")
+    f.create_dataset("label", labels.shape, dtype="i4")
     f["data"][:] = data.astype("f8")
-    f["label"][:] = labels.astype("f8")
+    f["label"][:] = labels.astype("i4")
     f.close
 
-data, labels = data_and_labels()
-np.random.shuffle(data)
-np.random.shuffle(labels)
-
+np.random.seed(42)
+data, labels = data_and_labels(data_matrix_from_csv("faces.csv"))
 save_dataset_with("faces-train.h5", data[0:260], labels[0:260])
 save_dataset_with("faces-test.h5", data[260:], labels[260:])
